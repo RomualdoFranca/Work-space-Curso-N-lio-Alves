63c2dc0e7551d323a684ef9c1b262d0a3e6ece1c
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-08-27 19:26:07 -0300

    aula sobre polimorfismo. Exercicio resolvido

--- src/secao13/polimorfismo/Application.java
+++ src/secao13/polimorfismo/Application.java
@@ -1,5 +1,8 @@
 package secao13.polimorfismo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import secao13.polimorfismo.entities.Employee;
 import secao13.polimorfismo.entities.OutsourcedEmployee;
 
@@ -7,13 +10,83 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		Employee employee01 = new Employee("Alex", 50, 20.00);
-		System.out.println(employee01);
-		System.out.println();
+		Scanner sc = new Scanner(System.in);
 		
-		OutsourcedEmployee employee02 = new OutsourcedEmployee("Bob", 100, 15.00, 200.00);
-		System.out.println(employee02);
+		ArrayList<Employee> employees = new ArrayList<Employee>();
+		
+		System.out.print("Enter the number of employees: ");
+		int numberEmployees = sc.nextInt();
+		sc.nextLine();//consome quebra de linha
+		
+		//loop para inserir os dados dos empregados
+		for (int i = 1; i <= numberEmployees; i++) {
+			
+			System.out.println("Employee #" + i + " data:");
+			String outsourced = null;
+			
+			String typeEmployee  = typeEmployee(outsourced, sc);
+			
+			//insere dados do empregado do tipo outsourced
+			if(typeEmployee.equals("y")) {
+				System.out.print("Name: ");
+				String name = sc.nextLine();
+				
+				System.out.print("Hours: ");
+				int hours = sc.nextInt();
+				sc.nextLine();
+				
+				System.out.print("Value per hour: ");
+				double valuePerHour = sc.nextDouble();
+				sc.nextLine();
+				
+				System.out.print("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				sc.nextLine();
+				//cria um objeto e adiciona na lista
+				employees.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+				
+			//insere dados do empregado do tipo comum
+			}else {
+				System.out.print("Name: ");
+				String name = sc.nextLine();
+				
+				System.out.print("Hours: ");
+				int hours = sc.nextInt();
+				sc.nextLine();
+				
+				System.out.print("Value per hour: ");
+				double valuePerHour = sc.nextDouble();
+				sc.nextLine();
+				employees.add(new Employee(name, hours, valuePerHour));//cria um objeto e adiciona na lista
+			}//fim do if-else
+			
+		}//fim do loop
+		
+		for (Employee employee : employees) {
+			System.out.println(employee);
+		}
+		
+		
+		sc.close();
 		
 	}
+	
+	//metodo que define o tipo de empregado
+	public static String typeEmployee(String type, Scanner sc) {
+		do {
+			System.out.println("Outsourced? (y/n)?");
+		    type = sc.nextLine();
+		    if(!type.equals("y") && !type.equals("n"))
+		    	System.out.println("Entrada inválida");
+		    
+		} while (!type.equals("y") && !type.equals("n"));
+		return type;
+		
+	}
+	
+	public static void employeeData() {
+		
+	}
+	
 
 }
